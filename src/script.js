2e7dea4baf09ccfe3c140e3eb03dd708954bec31
Anand <87514488+httpanand@users.noreply.github.com>
2021-12-10 21:46:29 +0530

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date("Dec 24, 2021 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("counter").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("counter").innerHTML = "The Event has expired !";
+    }
+}, 1000);
